02c5a5af6e4d62785eaff3fca1fb7ec985003f6f
SaaW008 <saad.achnani@gmail.com>
2023-03-17 16:54:47 -0700

    Update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - numbers
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 0 ; n < 10 ; n++)
+{
+printf("%d", n);
+}
+putchar('\n');
+return (0);
+}
