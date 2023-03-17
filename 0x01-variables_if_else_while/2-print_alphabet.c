0bd4b02ccfe63a16c039a38c4cb61e93332358fd
SaaW008 <saad.achnani@gmail.com>
2023-03-17 16:28:38 -0700

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets game
+ *
+ * Return: Always 0 (successe)
+ *
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
