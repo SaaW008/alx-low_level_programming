1d359671f070762f5075faa002813266067f9387
SaaW008 <saad.achnani@gmail.com>
2023-03-21 15:58:21 -0700

    Update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet lower case as string
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and error is set appropriately.
+ */
+
+void print_alphabet(void)
+{
+	int i,c = 10;
+
+	while (c > 0)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		c--;
+	}
+
+}
