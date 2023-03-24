088fe65079a51e30e79d4e0e3b18c5d7767a52c5
SaaW008 <saad.achnani@gmail.com>
2023-03-23 19:10:32 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check the character is upper or not
+ *
+ * Retune: 1 if upper, 0 if not
+ */
+
+int _isupper(int c)
+{
+	if ( c > 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
