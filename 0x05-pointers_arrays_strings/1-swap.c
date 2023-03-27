50a423760fa3bde7a9d258dbe94c96cfa4b15e23
SaaW008 <saad.achnani@gmail.com>
2023-03-27 16:30:42 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swip the values of two ints
+ * @a: fist int
+ * @b: second int
+ * Return: void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *p;
+
+	*p = *a;
+	*a = *b;
+	*b = *p;
+
+}
