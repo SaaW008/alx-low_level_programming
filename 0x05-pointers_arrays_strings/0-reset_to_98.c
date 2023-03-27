6780ae338793c89da8ced6aecd69de92a0a7decc
SaaW008 <saad.achnani@gmail.com>
2023-03-27 15:42:00 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - function that reset a vaiable by reference
+ * @n: the adress of the vabiable to reset
+ * Return: void.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
