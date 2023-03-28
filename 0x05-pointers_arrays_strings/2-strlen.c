527c631a120803214f79b011243b4efba5dd628a
SaaW008 <saad.achnani@gmail.com>
2023-03-27 17:38:06 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return the lenght of a string
+ * @s: adress of string variable
+ * Return: lenght of srting, 0 if no char exist
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	return (l);
+
+}
