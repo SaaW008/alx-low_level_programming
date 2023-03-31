0e25716f793e1b077b3ef9e3a45ad5ca854ae087
SaaW008 <saad.achnani@gmail.com>
2023-03-30 19:27:27 -0700

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int lsrc,ldest;
-	int i = 0;
+	int i, j;
 
-	lsrc = strlen(src);
-	ldest = strlen(dest) - 1;
-	while ( i <= lsrc )
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
 	{
-		dest[ldest + i] = src[i];
+		dest[i] = src[j];
+		j++;
 		i++;
 	}
-	dest[i]= '\0';
+
+	dest[i] = '\0';
+
 	return (dest);
 }
