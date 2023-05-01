9935db191f5c73e8e681ce86026bb6c66229d7f2
SaaW008 <saad.achnani@gmail.com>
2023-04-30 19:13:22 -0700

    Update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,23 +6,18 @@
  * a node at the begining of a listint_t list
  * @head: listint_t passed as a argument
  * @n: int value to add with a new node
- * Return: size_t of counted items, 1 if failed
+ * Return: size_t of counted items, NULL if failed
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *nn;
+	listint_t *nn = NULL;
 
 	nn = malloc(sizeof(listint_t));
+	if (nn == NULL)
+		return (NULL);
 	nn->n = n;
 	nn->next = *head;
-	if (nn->next != NULL)
-	{
-		*head = nn;
-		return (nn);
-	}
-	else
-	{
-		return (NULL);
-	}
+	*head = nn;
+	return (nn);
 }
