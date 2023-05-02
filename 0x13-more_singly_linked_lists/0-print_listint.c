5578ff655c11db0c6635744f15b16ac186cc512b
SaaW008 <saad.achnani@gmail.com>
2023-05-01 17:14:50 -0700

    Update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,14 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
 	}
 	return (i);
 }
