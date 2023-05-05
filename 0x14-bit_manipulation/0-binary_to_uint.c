7e6d885face94be1b16b8caf0f1fd627d29163a1
SaaW008 <saad.achnani@gmail.com>
2023-05-04 18:07:25 -0700

    Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,32 @@
 #include "main.h"
 
 /**
- * _putchar -
- * @c: The character to print
+ * binary_to_unit - convert a const binary to decimal form
+ * @b: pointer to binary char
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success decimal form.
+ * On error, 0 is returned.
  */
 
-int _putchar(char c)
+unsigned int binary_to_uint(const char *b)
 {
-	return (write(1, &c, 1));
+	int i = 0, cnt = 0, j = 1;
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if ((b[i] - '0') != 0 && (b[i] - '0') != 1)
+			return (0);
+		cnt++;
+		i++;
+	}
+	for (i = cnt - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			result += j;
+		j *= 2;
+	}
+	return (result);
 }
