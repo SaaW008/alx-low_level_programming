daf89c1793f9b0797be468f9e7050ffc1e3ae760
SaaW008 <saad.achnani@gmail.com>
2023-05-06 09:20:46 -0700

    Update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+/**
+ * union my_union - union to test values
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+ typedef union my_union_u
+        {
+                uint8_t u8;
+                uint16_t u16;
+                uint32_t u32;
+                uint64_t u64;
+        } my_union;
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
