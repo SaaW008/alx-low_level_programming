6eb3acadf2635e1ecf5289ba207ecabb50b135cc
SaaW008 <saad.achnani@gmail.com>
2023-09-04 10:45:40 -0700

    Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,24 +10,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int len = 0, count = 0, sum = 0;
+	unsigned int len = 0, count = 0, sum = 0;
 
-  if (b == NULL)
-    return (0);
+	if (b == NULL)
+		return (0);
 
-  len = _strlen(b);
-  while (len--)
-    {
-      if (b[len] != 48 && b[len] != 49)
-	return (0);
+	len = _strlen(b);
+	while (len--)
+	{
+		if (b[len] != 48 && b[len] != 49)
+			return (0);
 
-      if (b[len] == 49)
-	sum += 1 << count;
+		if (b[len] == 49)
+			sum += 1 << count;
 
-      count++;
-    }
+		count++;
+	}
 
-  return (sum);
+	return (sum);
 }
 
 /**
@@ -38,10 +38,10 @@ unsigned int binary_to_uint(const char *b)
  */
 int _strlen(const char *s)
 {
-  int c = 0;
+	int c = 0;
 
-  while (s[c])
-    c++;
+	while (s[c])
+		c++;
 
-  return (c);
+	return (c);
 }
