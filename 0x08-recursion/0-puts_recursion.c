f90b9b37002e143eed2e20a50c65cc6f8643e003
SaaW008 <saad.achnani@gmail.com>
2023-10-25 16:50:50 -0700

    Update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	if *s[0] = "/0"
+	if ( s[0] == '\0')
 	     {
-		     _putchar("/n");
-		     return 0;
+		     _putchar('\n');
+		     return;
 	     }
-	_putchar(*s[0]);
+	_putchar(s[0]);
 	++s;
-	_puts_recursion(*s);
+	_puts_recursion(s);
 }
