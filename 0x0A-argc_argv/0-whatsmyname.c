def5f225715789701077b9d3321eca7e593da7ac
SaaW008 <saad.achnani@gmail.com>
2023-10-28 17:28:49 -0700

    Update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: exit success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return 0;
+}
