fa0ed5bfff05a7a985562369c0ceb9a62d6391a4
SaaW008 <saad.achnani@gmail.com>
2023-11-01 16:09:24 -0700

    Update

--- 0x0B-malloc_free/1-strdup_backup.c
+++ 0x0B-malloc_free/1-strdup_backup.c
@@ -9,16 +9,22 @@
  */
 char *_strdup(char *str)
 {
-	char *ret;
-	unsigned int size;
+	char *res;
+	int i = 0, j;
 
 	if (str == NULL)
 		return (NULL);
-	size = 0;
-	while (str[size] != '\0')
-		size++;
-	ret = malloc(sizeof(char) * size);
-	if (ret == NULL)
+	while (str[i])
+	{
+		i++;
+	}
+	i++;
+	res = malloc(i * sizeof(char));
+	if (res == NULL)
 		return (NULL);
-	return (ret);
+	for (j = 0; j <= i; j++)
+	{
+		res[j] = str[j];
+	}
+	return (res);
 }
