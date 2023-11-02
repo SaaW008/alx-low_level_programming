65229cefd55f2cc6e67ff8f11db7aa99544becb6
SaaW008 <saad.achnani@gmail.com>
2023-11-02 15:18:51 -0700

    Update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that check malloc
+ * @b: size
+ * Return: no return
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	if ( b == NULL)
+		return (NULL);
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+}
