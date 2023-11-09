b1fab79ecc0b05f18084075b4199fbb8842ed5a1
SaaW008 <saad.achnani@gmail.com>
2023-11-09 15:04:06 -0800

    Update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,13 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (!(strcmp(s, ops[i].op)))
 			return (ops[i].f);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,18 +12,20 @@
 int main(int argc, char *argv[])
 {
 	int (*opt)(int, int);
+	int result;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
+		printf("Error1\n");
 		return (1);
 	}
 	opt = get_op_func(argv[2]);
 	if (opt == NULL)
 	{
-		printf("Error\n");
+		printf("Error2\n");
 		return (1);
 	}
-	printf("%d\n", opt(atoi(argv[1]), atoi(argv[3])));
+	result = opt(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
 	return (0);
 }
