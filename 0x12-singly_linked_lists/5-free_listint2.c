6f79285ffec3afb31198d89ce7ab708b2c423452
SaaW008 <saad.achnani@gmail.com>
2023-11-16 11:48:31 -0800

    Update

---
+++ 0x12-singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - a function to free a linked list
+ * passed as double pointer
+ * @head: head passed as argument
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *cursor = *head;
+	listint_t *prev = NULL;
+
+	if (*head != NULL)
+	{
+		while (cursor != NULL)
+		{
+			prev = cursor;
+			cursor = cursor->next;
+			free(prev);
+			*head = cursor;
+		}
+	}
+	else
+	{
+		return;
+	}
+	free(*head);
+	head = NULL;
+}
