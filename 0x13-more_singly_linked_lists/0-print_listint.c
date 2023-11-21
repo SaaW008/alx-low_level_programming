bcb0697a0b58df7d0cc4141366b02a1e5d4c8668
SaaW008 <saad.achnani@gmail.com>
2023-11-21 14:03:38 -0800

    Update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints
+ * all the elements of a list_t list
+ * @h: list_t passed as a argument
+ * Return: size_t of counted items
+ */
+
+size_t print_listint(const list_t *h)
+{
+	size_t i = 0;
+
+	if (h != NULL)
+	{
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
